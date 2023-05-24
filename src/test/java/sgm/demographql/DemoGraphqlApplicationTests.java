5a5f19f3b5a653f35708ac404a0662e245ccd887
sergiogmoraleda <sergio.g.moraleda@gmail.com>
2023-05-24 12:11:47 +0200

    comienzo

---
+++ src/test/java/sgm/demographql/DemoGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package sgm.demographql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
